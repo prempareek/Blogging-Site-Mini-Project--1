b5f16707548ea7547f16875b3fa02d99fccad8f1
prempareek <prempareekech@gmail.com>
2022-03-21 08:41:04 +0530

    Blog mini-project done

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect("mongodb+srv://SaloniKuralkar:Saloni%4030@cluster0.ui8kc.mongodb.net/Saloni_Projects-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
+mongoose.connect("mongodb+srv://premjoshi:CAeCnXX70lt2hET5@cluster0.7vw4v.mongodb.net/new?authSource=admin&replicaSet=atlas-10yy5l-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", {
+        useNewUrlParser: true
+    })
     .then(() => console.log("MongoDb is connected"))
     .catch(err => console.log(err))
 app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
